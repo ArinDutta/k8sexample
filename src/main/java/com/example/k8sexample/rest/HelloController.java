a2105311be49a6f84301bd76f361b0eea0ca316f
c15436 <c15436@P50-PC0DZ13Q.corp.us.int.rci.com>
2020-02-19 15:54:39 -0500

    GooGle Cloud Kubernets Example

--- src/main/java/com/example/k8sexample/rest/HelloController.java
+++ src/main/java/com/example/k8sexample/rest/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.k8sexample.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	
+	@Value("${welcome.message}")
+	private String welcomeMessage;
+	
 	@GetMapping("/hello/cloud")
 	public ResponseEntity<String> sayHello() {
-		return new ResponseEntity<>("Hello example", HttpStatus.OK);
+		return new ResponseEntity<>(welcomeMessage, HttpStatus.OK);
 	}
 
 }
